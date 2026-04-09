c4986a75eb17650602a48e937fa5950dd5eecfc9
Görkem Paradise <smailgorkem@gmail.com>
2026-04-09 22:43:21 +0300

    9 April 2026

---
+++ python/leetcode/array/0228_sumRanges.py
@@ -0,0 +1,18 @@
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        ans = []
+        i = 0
+
+        while i < len(nums):
+            S = nums[i]
+
+            while i + 1 < len(nums) and nums[i] + 1 == nums[i + 1]:
+                i += 1
+
+            if S != nums[i]:
+                ans.append(f"{S}->{nums[i]}")
+            else:
+                ans.append(str(S))
+            
+            i += 1
+        return ans
