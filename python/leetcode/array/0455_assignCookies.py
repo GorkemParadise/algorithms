965bed9bd4b7a7a4a25003e6adedd6eb635163e9
Görkem Paradise <smailgorkem@gmail.com>
2026-04-11 23:44:22 +0300

    11 April 2026

---
+++ python/leetcode/array/0455_assignCookies.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+
+        i = 0 # çocuk
+        j = 0 # kurabiye
+
+        while i < len(g) and j < len(s):
+            if s[j] >= g[i]:
+                i += 1
+            j += 1
+
+        return i
+                
